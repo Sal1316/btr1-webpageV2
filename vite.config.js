1629e5bd955e1b26c543db1b31dc06b139e77b3f
Sal Ceja <salceja45@gmail.com>
2024-09-19 10:47:23 -0700

    test7

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/btr1-webpageV2/", // This should match your repository name
+});
