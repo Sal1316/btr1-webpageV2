dc6d6f759913fea671191bf446a6fb83f6a9a611
Sal Ceja <salceja45@gmail.com>
2024-09-20 11:31:53 -0700

    predeploy to gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-
 // import { createRoot } from "react-dom/client";
 // import App from "./App.jsx";
 // import "./index.css";
@@ -9,13 +8,13 @@
 //   </StrictMode>
 // );
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/btr1-webpageV2/", // This should match your repository name
 });
